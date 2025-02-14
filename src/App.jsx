7b4a476d972106068f79fbc258d4ee88d6e44118
KarthikJayaramSK <sec23cj004@sairamtap.edu.in>
2025-02-14 23:56:59 +0530

    karthi

--- src/App.jsx
+++ src/App.jsx
@@ -4,11 +4,25 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  let name = "Karthik Jayaram S K"
 
-  return (
-    <h1>KARTHIK JAYARAM S K</h1>
+  const infoDetails={ 
+    name: "Karthik Jayaram S K",
+    age: 18,
+    Roll :43,
+    dept: "M.tech CSE"
+  }
+  alert(`my name is ${infoDetails.name}`)
+  console.log("My name is"+{name})
+  return ( 
+    <>
+      <h1 style={{textAlign:"center"}}>My Department is {infoDetails.dept}</h1> 
+      <p className='style'>Name: {infoDetails.name}<br/> 
+        Age: {infoDetails.age}<br/>
+        Roll:{infoDetails.Roll}<br/>
+        Department: {infoDetails.dept}<br/>
+      </p>
+    </>
   )
 }
-
 export default App
